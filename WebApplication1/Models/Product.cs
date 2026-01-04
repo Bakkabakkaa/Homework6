467f235ea0c13bbcfb9bd30b404058cb3d45735d
Art'm <kasko.artem45@gmail.com>
2026-01-04 20:45:09 +0300

    add Product model

---
+++ WebApplication1/Models/Product.cs
@@ -0,0 +1,10 @@
+namespace WebApplication1.Models;
+
+public class Product
+{
+    public string ID { get; set; }
+    public string Category { get; set; }
+    public string Description { get; set; }
+    public string Price { get; set; }
+    public string Quantity { get; set; }
+}
